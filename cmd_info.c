5c1dd002be7dd35200e4372d9977173f30311c78
FlyingRainyCats <161649702+FlyingRainyCats@users.noreply.github.com>
2024-08-07 19:06:53 +0100

    feat: support for verbose info flag

--- cmd_info.c
+++ cmd_info.c
@@ -1,33 +1,50 @@
+#include <unistd.h>
+
 #include "pmlxzj.h"
-#include "pmlxzj_utils.h"
 #include "pmlxzj_commands.h"
+
+struct pmlxzj_cmd_print_info_param_t {
+  bool verbose;
+};
 
 pmlxzj_enumerate_state_e enum_frame_print_info(pmlxzj_state_t* app,
                                                pmlxzj_frame_info_t* frame,
                                                void* extra_callback_data) {
-  PMLXZJ_UNUSED_PARAMETER(extra_callback_data);
+  struct pmlxzj_cmd_print_info_param_t* param = extra_callback_data;
 
   long frame_start_offset = ftell(app->file);
   if (frame->compressed_size > 10240) {
     long patch_start_offset = frame_start_offset + (long)frame->compressed_size / 2;
     printf("frame #%d, image #%d (len=(0x%x, 0x%x), offset=0x%x, patch=0x%x)\n", frame->frame_id, frame->image_id,
            frame->compressed_size, frame->decompressed_size, (int)frame_start_offset, (int)patch_start_offset);
-  } else {
-#ifndef NDEBUG
+  } else if (param->verbose) {
     printf("frame #%d, image #%d (len=(0x%x, 0x%x), offset=0x%x)\n", frame->frame_id, frame->image_id,
            frame->compressed_size, frame->decompressed_size, (int)frame_start_offset);
-#endif
   }
 
   return PMLXZJ_ENUM_CONTINUE;
 }
 
 int pmlxzj_cmd_print_info(int argc, char** argv) {
-  if (argc <= 2) {
+  struct pmlxzj_cmd_print_info_param_t param = {0};
+  int option = -1;
+  while ((option = getopt(argc, argv, "v")) != -1) {
+    if (option == 'v') {
+      param.verbose = true;
+    } else {
+      fprintf(stderr, "ERROR: unknown option '-%c'\n", optopt);
+      pmlxzj_usage(argv[0]);
+      return 1;
+    }
+  }
+
+  if (optind >= argc) {
+    fprintf(stderr, "ERROR: 'input' required.\n");
     pmlxzj_usage(argv[0]);
     return 1;
   }
-  FILE* f_src = fopen(argv[2], "rb");
+
+  FILE* f_src = fopen(argv[optind], "rb");
   if (f_src == NULL) {
     perror("ERROR: open source input");
     return 1;
@@ -51,15 +68,11 @@ int pmlxzj_cmd_print_info(int argc, char** argv) {
 
   printf("offset_data_start: 0x%x\n", app.footer.offset_data_start);
   printf("frame offset: 0x%lx\n", app.first_frame_offset);
-  if (app.audio_start_offset) {
-    printf("audio offset: 0x%lx\n", app.audio_start_offset);
-  } else {
-    printf("no audio\n");
-  }
+  printf("audio offset: 0x%lx\n", app.audio_start_offset);
 
   printf("encrypt mode 1 nonce: %d\n", app.footer.mode_1_nonce);
   printf("encrypt mode 2 nonce: %d\n", app.footer.mode_2_nonce);
+  pmlxzj_enumerate_images(&app, enum_frame_print_info, &param);
 
-  pmlxzj_enumerate_images(&app, enum_frame_print_info, NULL);
   return 0;
 }
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include "pmlxzj_commands.h"
 #include "pmlxzj_win32.h"
 
@@ -12,6 +13,7 @@
 #endif
 
 int main(int argc, char** argv) {
+  optind++;
   FixWindowsUnicodeSupport(&argv);
 
   printf("pmlxzj_unlocker v" PROJECT_VERSION " by 爱飞的猫@52pojie.cn (FlyingRainyCats)\n");
